4a352552e3c3df6e169bb1dc88116ed7b68f6eab
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-28 17:47:13 -0300

    feat: implement home page layout with title, text, and buttons.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,22 @@
-import { Text } from "react-native";
-
+import { Title, Text, Box, Button, Spacer } from '../../components'
 
 export function Home() {
     return (
-        <Text>Hello World</Text>
+        <Box justify="center" hasPadding align="center" background="gray50">
+            <Box justify="center" align="center" fluid>
+                <Title color="light" variant="big" bold>LOOKAPP</Title>
+                <Spacer />
+                <Text align="center" spacing="0px 40px" variant="big" bold>Fique por dentro do mundo da moda
+                    e compre seus looks favoritos.</Text>
+            </Box>
+            <Box justify="flex-end" align="center" fluid>
+                <Button block>
+                    <Text color="light" variant="big">Fazer login em minha conta</Text>
+                </Button>
+                <Spacer size="20px" />
+                <Text color="light" variant="big" underline onPress={(() => alert('teste'))}>Criar nova conta</Text>
+                <Spacer size="70px" />
+            </Box>
+        </Box>
     )
 }
