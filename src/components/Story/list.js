c6781a27088daea0dc6673effd4e5c55b7aa3078
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-29 11:54:58 -0300

    feat: add StoryList component to display multiple stories horizontally.

---
+++ src/components/Story/list.js
@@ -0,0 +1,20 @@
+import { Text, Box, ScrollView } from '../../components'
+import { Story } from './index'
+
+export function StoryList() {
+    return (
+        <Box fluid>
+            <Box row fluid justify="space-between" hasPadding height="60px">
+                <Text bold color="dark">Stories</Text>
+                <Text bold color="danger" underline>Mostrar tudo</Text>
+            </Box>
+            <ScrollView key="id" horizontal style={{
+                paddingLeft: 20,
+            }}>
+                {Array.from(Array(5))?.map(item => (
+                    <Story />
+                ))}
+            </ScrollView>
+        </Box>
+    )
+}
