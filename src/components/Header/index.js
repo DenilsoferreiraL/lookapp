5bef999bd137e0c30766e1849c978376ce566b85
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-29 11:17:59 -0300

    feat: add StatusBar component for improved UI customization.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,23 @@
-import { SafeAreaView } from 'react-native'
-import { Title, Text, Box, Button, Spacer, Input, Touchable } from '../../components'
+import { SafeAreaView, StatusBar } from 'react-native'
+import { Title, Box, Touchable } from '../../components'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { colors } from '../../styles/theme.json'
 
 export function Header({ title }) {
     return (
-        <Box fluid height="100px" justify="center" border={`1px solid ${colors.muted}50`}>
-            <SafeAreaView style={{ flexDirection: 'row' }}>
-                <Touchable width="80px" align="center" justify="center" onPress={(() => alert('teste'))}>
-                    <Icon name="menu" size={25} />
-                </Touchable>
-                <Box align="center" justify="center">
-                    <Title>{title}</Title>
-                </Box>
-                <Box width="80px"></Box>
-            </SafeAreaView>
-        </Box>
+        <>
+            <StatusBar barStyle="dark-content" />
+            <Box fluid height="100px" justify="center" border={`1px solid ${colors.muted}50`} >
+                <SafeAreaView style={{ flexDirection: 'row' }} >
+                    <Touchable width="80px" align="center" justify="center" onPress={(() => alert('teste'))}>
+                        <Icon name="menu" size={25} />
+                    </Touchable>
+                    <Box align="center" justify="center">
+                        <Title variant="normal">{title}</Title>
+                    </Box>
+                    <Box width="80px"></Box>
+                </SafeAreaView>
+            </Box>
+        </>
     )
 }
