fcc50b6ac51e9485367fbc831d219d32e54bc6f0
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-11-30 09:12:45 -0300

    feat: add ScrollView to display StoryList and PostList with spacing.

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,12 +1,17 @@
-import { Box } from '../../components'
+import { Box, ScrollView, Spacer } from '../../components'
 import { Header } from '../../components/Header'
+import { PostList } from '../../components/Post/list'
 import { StoryList } from '../../components/Story/list'
 
 export function Feed() {
     return (
         <Box background="light"  >
             <Header title={'Explorar'} />
-            <StoryList />
+            <ScrollView>
+                <StoryList />
+                <Spacer />
+                <PostList />
+            </ScrollView>
         </Box>
     )
 }
