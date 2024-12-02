d0df5a92b4758e321ff913bd7d49a659d2cacff2
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-02 10:52:58 -0300

    feat: implement navigation to open Drawer with useNavigation hook.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,21 @@ import { SafeAreaView, StatusBar } from 'react-native'
 import { Title, Box, Touchable } from '../../components'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { colors } from '../../styles/theme.json'
+import { useNavigation } from "@react-navigation/native";
 
 export function Header({ title }) {
+    const navigation = useNavigation()
+
     return (
         <>
             <StatusBar barStyle="dark-content" />
             <Box fluid height="100px" justify="center" border={`1px solid ${colors.muted}50`} >
                 <SafeAreaView style={{ flexDirection: 'row' }} >
-                    <Touchable width="80px" align="center" justify="center" onPress={(() => alert('teste'))}>
+                    <Touchable
+                        width="80px"
+                        align="center"
+                        justify="center"
+                        onPress={() => navigation.openDrawer()}>
                         <Icon name="menu" size={25} />
                     </Touchable>
                     <Box align="center" justify="center">
