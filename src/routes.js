01e49607788d6819db57a42d758828a0da5ff7c3
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-02 10:51:48 -0300

    feat: integrate Drawer navigation to navigate to Feed screen.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import { Home } from "./pages/Home";
 import { Signin } from "./pages/Signin";
 import { SingUp } from "./pages/Singup";
 import { Feed } from "./pages/Feed";
+import { DrawerNavigate } from "./components/Drawer";
 
 const Stack = createStackNavigator()
 
@@ -14,7 +16,7 @@ export function Routes() {
                 <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
                 <Stack.Screen options={{ headerShown: false }} name="Signin" component={Signin} />
                 <Stack.Screen options={{ headerShown: false }} name="Singup" component={SingUp} />
-                <Stack.Screen options={{ headerShown: false }} name="Feed" component={Feed} />
+                <Stack.Screen options={{ headerShown: false }} name="Feed" component={DrawerNavigate} />
             </Stack.Navigator>
         </NavigationContainer>
     )
