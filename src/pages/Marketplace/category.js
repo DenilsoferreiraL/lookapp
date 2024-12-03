a3607ee9452b9edde43834dd51a51dccea913940
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-03 12:05:57 -0300

    feat: create Category component with basic layout and text.

---
+++ src/pages/Marketplace/category.js
@@ -0,0 +1,10 @@
+import { Box, Text } from "../../components";
+
+
+export function Category() {
+    return (
+        <Box hasPadding align="center" justify="center" background="light">
+            <Text>Categoria</Text>
+        </Box>
+    )
+}
