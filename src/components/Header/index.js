a205740385700cea6a971fc746bde02f1d634917
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-03 12:03:25 -0300

    refactor: update Header to use View with styled border and background.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,42 @@
-import { SafeAreaView, StatusBar } from 'react-native'
+import { SafeAreaView, StatusBar, View } from 'react-native'
 import { Title, Box, Touchable } from '../../components'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { colors } from '../../styles/theme.json'
 import { useNavigation } from "@react-navigation/native";
+import util from '../../util';
 
-export function Header({ title }) {
+export function Header({ title = 'Explorar', right = null }) {
     const navigation = useNavigation()
 
     return (
-        <>
+        <View style={{
+            borderBottomWidth: 1,
+            borderBottomStyle: 'solid',
+            borderBottomColor: util.toAlpha(colors.muted, 50),
+            backgroundColor: colors.light
+        }} >
             <StatusBar barStyle="dark-content" />
-            <Box fluid height="100px" justify="center" border={`1px solid ${colors.muted}50`} >
-                <SafeAreaView style={{ flexDirection: 'row' }} >
-                    <Touchable
-                        width="80px"
-                        align="center"
-                        justify="center"
-                        onPress={() => navigation.openDrawer()}>
-                        <Icon name="menu" size={25} />
-                    </Touchable>
-                    <Box align="center" justify="center">
-                        <Title variant="normal">{title}</Title>
-                    </Box>
-                    <Box width="80px"></Box>
-                </SafeAreaView>
-            </Box>
-        </>
+            <SafeAreaView
+                style={{
+                    flexDirection: 'row',
+                    width: '100%',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}
+            >
+                <Touchable
+                    width="70px"
+                    align="center"
+                    justify="center"
+                    hasPadding
+                    onPress={() => navigation.openDrawer()}>
+                    <Icon name="menu" size={25} />
+                </Touchable>
+                <Box align="center" justify="center">
+                    <Title variant="normal">{title}</Title>
+                </Box>
+                {right ? right() : <Touchable width="70px" hasPadding></Touchable>}
+            </SafeAreaView>
+        </View>
     )
 }
