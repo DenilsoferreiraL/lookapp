8b23315ebf26231173dc8b8843a70423910d3a84
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-03 12:04:07 -0300

    refactor: dynamic DrawerNavigate implementation with reusable screens and icons.

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,56 +1,82 @@
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
 import { Feed } from "../../pages/Feed";
-import { Home } from "../../pages/Home";
 import { Signin } from "../../pages/Signin";
+import { Marketplace } from "../../pages/Marketplace";
+import { Category } from "../../pages/Marketplace/category";
+import { Product } from "../../pages/Marketplace/product";
 import { colors } from '../../styles/theme.json';
-import Icon from 'react-native-vector-icons/SimpleLineIcons'
-import { Title, Spacer } from '../../components/index'
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import { Title, Spacer } from '../../components/index';
 import util from "../../util";
 
 const Drawer = createDrawerNavigator();
 
-const CustomDrawerComponent = props => {
+
+const drawerScreens = [
+    {
+        name: "Início",
+        component: Feed,
+        icon: "home",
+    },
+    {
+        name: "Vitrine",
+        component: Marketplace,
+        icon: "bag",
+    },
+
+    {
+        name: "Produtos",
+        component: Product,
+        icon: "tag",
+    },
+    {
+        name: "Categorias",
+        component: Category,
+        icon: "grid",
+    },
+    {
+        name: "Pedidos",
+        component: Signin,
+        icon: "basket",
+    },
+];
+
+const CustomDrawerComponent = (props) => {
     return (
         <DrawerContentScrollView {...props}>
-            <Title bold color="light" variant="big" hasPadding >LOOKAPP</Title>
+            <Title bold color="light" variant="big" hasPadding>
+                LOOKAPP
+            </Title>
             <Spacer />
             <DrawerItemList {...props} />
-        </DrawerContentScrollView >
-    )
-}
+        </DrawerContentScrollView>
+    );
+};
 
 export function DrawerNavigate() {
     return (
         <Drawer.Navigator
-            drawerContent={props => <CustomDrawerComponent {...props} />}
+            drawerContent={(props) => <CustomDrawerComponent {...props} />}
             screenOptions={{
                 headerShown: false,
                 drawerStyle: {
                     backgroundColor: colors.black,
                 },
-                drawerActiveBackgroundColor: util.toAlpha(colors.primary, 60),
+                drawerActiveBackgroundColor: colors.primary,
                 drawerActiveTintColor: colors.light,
-                drawerInactiveTintColor: util.toAlpha(colors.light, 60)
+                drawerInactiveTintColor: util.toAlpha(colors.light, 60),
             }}
         >
-            <Drawer.Screen
-                name="Início"
-                component={Feed}
-                options={{
-                    drawerIcon: ({ focused, color }) => (
-                        <Icon name="people" color={color} size={20} />
-                    )
-                }} />
-            <Drawer.Screen name="Vitrine" component={Home} options={{
-                drawerIcon: ({ focused, color }) => (
-                    <Icon name="tag" color={color} size={20} />
-                )
-            }} />
-            <Drawer.Screen name="Pedidos" component={Signin} options={{
-                drawerIcon: ({ focused, color }) => (
-                    <Icon name="basket" color={color} size={20} />
-                )
-            }} />
+            {drawerScreens.map(({ name, component, icon }) => (
+                <Drawer.Screen
+                    key={name}
+                    name={name}
+                    component={component}
+                    options={{
+                        drawerIcon: ({ color }) => <Icon name={icon} color={color} size={20} />,
+                    }}
+                />
+            ))}
         </Drawer.Navigator>
     );
 }
