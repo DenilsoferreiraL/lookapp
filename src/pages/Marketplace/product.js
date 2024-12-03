a27712d40dab9de78687bafd957f4700015c36d9
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-03 12:06:51 -0300

    feat: create Product page with basic structure.

---
+++ src/pages/Marketplace/product.js
@@ -0,0 +1,10 @@
+import { Box, Text } from "../../components";
+
+
+export function Product() {
+    return (
+        <Box align="center" justify="center" background="light">
+            <Text>Produtos</Text>
+        </Box>
+    )
+}
