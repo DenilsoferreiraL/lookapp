536bab58d7d81476924b64cb07516e297d355a12
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-12-03 12:05:06 -0300

    feat: create Category component with image, title, and description display.

---
+++ src/components/Category/index.js
@@ -0,0 +1,32 @@
+import { Box, Cover, Spacer, Text, Title, Touchable } from "../../components";
+import { colors } from '../../styles/theme.json'
+import women from '../../assets/women.png';
+import util from "../../util";
+
+export function Category({ title, description }) {
+    return (
+        <Touchable
+            width="100%"
+            height="180px"
+            radius="10px"
+            spacing="10px 0px">
+            <Cover
+                width="100%"
+                height="100%"
+                source={women}
+            >
+                <Box
+                    width="100%"
+                    justify="center"
+                    align="center"
+                    hasPadding
+                    background={util.toAlpha(colors.black, 0)}
+                >
+                    <Title color="light" >{title}</Title>
+                    <Spacer />
+                    <Text color="light">{description}</Text>
+                </Box>
+            </Cover>
+        </Touchable>
+    );
+}
