3227e127c08ec997071252d9a14675e45655862c
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-01-07 15:39:46 -0300

    feat: create Picker component for size selection with custom styles and state management

---
+++ src/components/Picker/index.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from "react-native";
+import { Box, Spacer, Text, Touchable } from "..";
+import { colors } from '../../styles/theme.json'
+import { useEffect, useState } from "react";
+
+export function Picker({ title = '', options = [], initialValue = '', onChange = value => { } }) {
+
+    const [selected, setSelected] = useState('')
+
+    useEffect(() => {
+        setSelected(initialValue)
+    }, [])
+
+    return (
+        <>
+            <Text color="dark" bold variant="big">{title}</Text>
+            <Spacer />
+            <Box row fluid height="50px">
+                {options?.map((opt, index) => (
+                    <Touchable
+                        onPress={() => [
+                            setSelected(opt?.value),
+                            onChange(opt?.value)
+                        ]}
+                        key={index}
+                        style={[styles.base, styles[selected === opt?.value ? 'checked' : 'unchecked']]}>
+                        <Text style={selected === opt?.value ? styles.textChecked : undefined}>{opt?.label}</Text>
+                    </Touchable>
+                ))}
+            </Box>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    base: {
+        maxWidth: 45,
+        height: 45,
+        borderRadius: 10,
+        marginRight: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    unchecked: {
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: colors.muted,
+    },
+    checked: {
+        backgroundColor: colors.primary,
+    },
+    textChecked: {
+        color: colors.light
+    }
+});
