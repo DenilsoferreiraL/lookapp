83f6ab028225f8885fb98144368e9885271f718c
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-01-08 08:43:38 -0300

    feat: hide header on Cart screen

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import { SingUp } from "./pages/Singup";
 import { DrawerNavigate } from "./components/Drawer";
 import { Category } from "./pages/Marketplace/category";
 import { Product } from "./pages/Marketplace/product";
+import { Cart } from "./pages/Cart";
 
 
 const Stack = createStackNavigator()
@@ -14,13 +15,14 @@ const Stack = createStackNavigator()
 export function Routes() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" >
+            <Stack.Navigator initialRouteName="Cart" >
                 <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
                 <Stack.Screen options={{ headerShown: false }} name="Signin" component={Signin} />
                 <Stack.Screen options={{ headerShown: false }} name="Singup" component={SingUp} />
                 <Stack.Screen options={{ headerShown: false }} name="Feed" component={DrawerNavigate} />
                 <Stack.Screen options={{ headerShown: false }} name="Category" component={Category} />
                 <Stack.Screen options={{ headerShown: false }} name="Product" component={Product} />
+                <Stack.Screen options={{ headerShown: false }} name="Cart" component={Cart} />
             </Stack.Navigator>
         </NavigationContainer>
     )
